b09a0f8d609e31fc256ac138323abc3b759247d2
yuriya <namyuri@gmail.com>
2021-11-30 21:23:05 +0900

    store /search? - add comment in route

--- src/route.js
+++ src/route.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
 
   router.get("/", lunchat.getAllStore);
   router.post("/", lunchat.addNewStore);
-  router.get("/search", lunchat.findStore);
+  router.get("/search", lunchat.findStore); //shall be before :id route
   router.get("/:id", lunchat.findStoreById);
   router.put("/:id", lunchat.updateStoreById);
   router.delete("/:id", lunchat.removeStore);
