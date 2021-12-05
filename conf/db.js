68b3e93ccc2156acf4dc3b4cbe675facce569e5b
yuriya <namyuri@gmail.com>
2021-12-05 14:57:44 +0900

    db.js comment out code clean-up

--- conf/db.js
+++ conf/db.js
@@ -15,35 +15,4 @@ const dbConnection = {
   },
 };
 
-// const dbConnection = {
-//   init: function () {
-//     return mysql.createConnection({
-//       host: process.env.host,
-//       port: process.env.port,
-//       user: process.env.user,
-//       password: process.env.password,
-//       database: process.env.database,
-//       dateStrings: "date",
-//     });
-//   },
-//   open: function (con) {
-//     con.connect((err) => {
-//       if (err) {
-//         utilFunc.printLog("MySQL connection fail : " + err);
-//       } else {
-//         utilFunc.printLog("MySQL Connected!!!");
-//       }
-//     });
-//   },
-//   close: function (con) {
-//     con.end((err) => {
-//       if (err) {
-//         utilFunc.printLog("MySQL close fail : " + err);
-//       } else {
-//         utilFunc.printLog("MySQL Terminated...");
-//       }
-//     });
-//   },
-// };
-
 module.exports = dbConnection;
